8b1bf97bd3072d3d10bb972f62af309d52cff2a7
rahul197vns <70707705+rahul197vns@users.noreply.github.com>
2025-08-06 18:54:32 +0530

    gen ai code added

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,61 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = var.region
+}
+
+variable "key_pair_name" {}
+variable "ssh_private_key" {}
+variable "region" {
+  default = "ap-south-1"
 }
 
 resource "aws_instance" "prod" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  key_name      = "your-key"
+  key_name      = var.key_pair_name
 
   tags = {
     Name = "genai-prod-ec2"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install -y nodejs npm git nginx docker.io",
+      "sudo systemctl start nginx",
+      "sudo systemctl enable nginx"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = var.ssh_private_key
+      host        = self.public_ip
+    }
   }
 }
 
 resource "aws_instance" "staging" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  key_name      = "your-key"
+  key_name      = var.key_pair_name
 
   tags = {
     Name = "genai-staging-ec2"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install -y nodejs npm git nginx docker.io",
+      "sudo systemctl start nginx",
+      "sudo systemctl enable nginx"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = var.ssh_private_key
+      host        = self.public_ip
+    }
   }
 }
