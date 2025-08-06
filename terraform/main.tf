5c6de64fd91863158bc1a6a7f43a129c874581af
rahul197vns <70707705+rahul197vns@users.noreply.github.com>
2025-08-06 20:02:06 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "prod" {
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file("~/.ssh/Gen-AI.pem")
+      private_key = file("/.ssh/Gen-AI.pem")
       host        = self.public_ip
     }
   }
